e17f9a833a26b92c093fc397bb6ccbb6d7e6220e
daveward72 <dwward1972@gmail.com>
2025-08-30 17:37:56 -0500

    Added --verbose flag

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 import os
 import sys
+import argparse
 
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
 
 def main():
-    if len(sys.argv) != 2:
-        print("Supply the prompt as a single command line argument.")
+    if len(sys.argv) < 2:
+        print("Supply the prompt as a single command line argument, optionally specifiying a --verbose flag.")
         sys.exit(1)
 
     prompt = sys.argv[1]
+    is_verbose = len(sys.argv) > 2 and sys.argv[2] == '--verbose'
 
     messages = [
     types.Content(role="user", parts=[types.Part(text=prompt)]),
@@ -23,9 +25,12 @@ def main():
         model='gemini-2.0-flash-001',
         contents=messages
     )
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if (is_verbose):
+        print(f"User prompt: {prompt}")
+    print(f"Response: {response.text}")
+    if (is_verbose):
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
     sys.exit(0)
 
