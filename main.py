888a6d3815c9a9fb7d131c619b3ab90e0fa1460c
daveward72 <dwward1972@gmail.com>
2025-08-30 17:17:31 -0500

    Update to send messages list with request

--- main.py
+++ main.py
@@ -3,20 +3,25 @@ import sys
 
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     if len(sys.argv) != 2:
-        print("Supply the prompt as a single command-line argument.")
+        print("Supply the prompt as a single command line argument.")
         sys.exit(1)
 
     prompt = sys.argv[1]
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=messages
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
