06e1d9b2c002b6b479b54eb66c462622a0e8c44c
daveward72 <dwward1972@gmail.com>
2025-08-30 17:12:24 -0500

    Take prompt from command line argument

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 import os
+import sys
+
 from dotenv import load_dotenv
 from google import genai
 
 def main():
+    if len(sys.argv) != 2:
+        print("Supply the prompt as a single command-line argument.")
+        sys.exit(1)
+
+    prompt = sys.argv[1]
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+        contents=prompt
     )
     print(response.text)
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    sys.exit(0)
 
 if __name__ == "__main__":
     main()
